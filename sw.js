72d90b1042797c817adcb7364894a33a2f84b5c2
shawntanyaradzwa2007-ship-it <shawntanyaradzwa2007@gmail.com>
2026-07-04 20:10:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'couple-wheel-v1';
+const urlsToCache = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.map(name => name !== CACHE_NAME && caches.delete(name)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
